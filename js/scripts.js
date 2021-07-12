c67436a75c6293d8b054189433f265fcf45dd69a
shadow-walker101 <otienokarani019@gmail.com>
2021-07-12 18:21:15 +0300

    Adds the user interface logic that take the user input and creates a contact object then return the first name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
+// business logic
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
 }
+
+//user interface logic
+
+$(document).ready(() => {
+    $("form#new-contact").submit((event) => {
+        event.preventDefault();
+
+        let inputtedFirstName = $("input#new-first-name").val();
+        let inputtedLastName = $("input#new-last-name").val();
+
+        let newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+        $("ul#contacts").append("<li><span class = 'contact'>" + newContact.firstName + "</span></li>");
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+    })
+})
