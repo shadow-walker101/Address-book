05bd6eaa7d149644b21fc1b54c1c81c24152e06a
shadow-walker101 <otienokarani019@gmail.com>
2021-07-14 16:10:30 +0300

    Adds the Address constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
+    this.addresses = [];
+}
+
+function Address(street, city, county) {
+    this.street = street;
+    this.city = city;
+    this.county = county;
 }
 
 Contact.prototype.fullName = function() {
